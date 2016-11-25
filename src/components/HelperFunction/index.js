3190b3a7d8f45200e0f774d5cf6b65c175196ab8
anwaukoni <anwaukoni@gmail.com>
2016-11-25 15:22:54 +0000

    added limit to the parameter to fetch url

--- src/components/HelperFunction/index.js
+++ src/components/HelperFunction/index.js
@@ -3,7 +3,7 @@ const fetch = require('isomorphic-fetch');
 
 export function search(searchTerm) {
   return new Promise(function (resolve, reject) {
-    fetch(`https://api.spotify.com/v1/search?q=${searchTerm}&type=track`)
+    fetch(`https://api.spotify.com/v1/search?q=${searchTerm}&type=track&limit=4`)
         .then(function(response) {
             if (response.status >= 400) {
                 throw new Error("Bad response from spotify");
